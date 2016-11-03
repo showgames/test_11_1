7d5f09898c811351c83e02d4bc60994a7528281f
林将平 <2329shohei@gmail.com>
2016-11-03 17:42:28 +0900

    STAGEクラスを作成 (フィールドを表現するクラス)

--- field.h
+++ field.h
@@ -1,3 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define ROW 5
+#define COL 5
+
 enum Field {
     WAL,
     GOA,
@@ -5,3 +12,30 @@ enum Field {
     BAG,
     EMP,
 };
+
+class STAGE
+{
+    vector<Field> stage;
+    int startPlace;
+
+    STAGE();
+    STAGE(int start, int size, Field* field);
+};
+
+STAGE::STAGE() 
+{
+    int i;
+    for (i = 0; i < ROW*COL; i++) {
+        stage.push_back(WAL);
+    }
+}
+
+STAGE::STAGE(int start, int size, Field* field)
+{
+    startPlace = start;
+    int i;
+    
+    for (i = 0; i < size; i++) {
+        stage.push_back(field[i]);
+    }
+}
